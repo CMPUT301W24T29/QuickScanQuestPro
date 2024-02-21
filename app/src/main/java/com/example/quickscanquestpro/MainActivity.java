69148cefd5aaab6ae5812ada2ff94782f6a21fca
DrawnActor <22815637+DrawnActor@users.noreply.github.com>
2024-02-20 21:15:41 -0700

    Set up navbar/project structure to work correctly

--- app/src/main/java/com/example/quickscanquestpro/MainActivity.java
+++ app/src/main/java/com/example/quickscanquestpro/MainActivity.java
@@ -30,43 +30,40 @@ public class MainActivity extends AppCompatActivity {
         // sets the default selected item for the main activity to the qrscanner button
         navBarView.setSelectedItemId(R.id.navigation_qr_scanner);
         // adds functions to the navbar button
-        navBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Log.i("NavMenu", "navButtonPressed: title is " + item.getTitle());
-                String pressedTitle = (String) item.getTitle();
+        navBarView.setOnItemSelectedListener(item -> {
+            Log.i("NavMenu", "navButtonPressed: title is " + item.getTitle());
+            String pressedTitle = (String) item.getTitle();
 
-                // gets the fragment currently loaded into the content view
-                Fragment callerFragment = getSupportFragmentManager().findFragmentById(R.id.content);
-                // gets the tag supplied to the fragment when displayed, which is the title of the button that opens it
-                String caller = callerFragment.getTag();
+            // gets the fragment currently loaded into the content view
+            Fragment callerFragment = getSupportFragmentManager().findFragmentById(R.id.content);
+            // gets the tag supplied to the fragment when displayed, which is the title of the button that opens it
+            String caller = callerFragment.getTag();
 
-                // gets the string resources for all the buttons
-                String dashboardTitle = callerFragment.getString(R.string.title_dashboard);
-                String qrTitle = callerFragment.getString(R.string.title_qr_scanner);
-                String profileTitle = callerFragment.getString(R.string.title_profile);
+            // gets the string resources for all the buttons
+            String dashboardTitle = callerFragment.getString(R.string.title_dashboard);
+            String qrTitle = callerFragment.getString(R.string.title_qr_scanner);
+            String profileTitle = callerFragment.getString(R.string.title_profile);
 
-                // if the button clicked is the same as the currently displayed fragment, do nothing!
-                if (Objects.equals(caller, pressedTitle)) {
-                    Log.i("NavMenu", "ignoring press on " + item.getTitle() + " because it was already active");
-                    return false;
-                }
-
-                Fragment fragment;
-                if (Objects.equals(pressedTitle, dashboardTitle)) {
-                    fragment = new EventDashboardFragment();
-                } else if (Objects.equals(pressedTitle, profileTitle)) {
-                    fragment = new ProfileFragment();
-                } else {
-                    // default to qr code home view
-                    fragment = new HomeViewFragment();
-                }
-
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.content, fragment, pressedTitle);
-                fragmentTransaction.commit();
-                return true;
+            // if the button clicked is the same as the currently displayed fragment, do nothing!
+            if (Objects.equals(caller, pressedTitle)) {
+                Log.i("NavMenu", "ignoring press on " + item.getTitle() + " because it was already active");
+                return false;
             }
+
+            Fragment fragment1;
+            if (Objects.equals(pressedTitle, dashboardTitle)) {
+                fragment1 = new EventDashboardFragment();
+            } else if (Objects.equals(pressedTitle, profileTitle)) {
+                fragment1 = new ProfileFragment();
+            } else {
+                // default to qr code home view
+                fragment1 = new HomeViewFragment();
+            }
+
+            FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction1.replace(R.id.content, fragment1, pressedTitle);
+            fragmentTransaction1.commit();
+            return true;
         });
 
     }
