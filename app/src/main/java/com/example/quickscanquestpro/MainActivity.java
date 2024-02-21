df93784ed577e2fc921a934d6abcb1103bde57ac
iftemum <iftemumraian@gmail.com>
2024-02-20 22:14:36 -0600

    Fixed issues with camera preview

--- app/src/main/java/com/example/quickscanquestpro/MainActivity.java
+++ app/src/main/java/com/example/quickscanquestpro/MainActivity.java
@@ -10,15 +10,29 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private QRCodeScanner qrCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_qr_scanner);
+
         PreviewView previewView = findViewById(R.id.cameraFeed);
-        new QRCodeScanner(this, this, previewView);
+        qrCodeScanner = new QRCodeScanner(this, previewView);
+        qrCodeScanner.startCamera();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        qrCodeScanner.shutdown();
     }
 }
