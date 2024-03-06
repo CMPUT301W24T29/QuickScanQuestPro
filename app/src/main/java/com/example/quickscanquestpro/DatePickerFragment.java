9b8c149ae8280e98c06bd69b8d315a4b6b0d050e
Alastair <22815637+DrawnActor@users.noreply.github.com>
2024-03-05 22:59:06 -0700

    javadocs for date/time picker

--- app/src/main/java/com/example/quickscanquestpro/DatePickerFragment.java
+++ app/src/main/java/com/example/quickscanquestpro/DatePickerFragment.java
@@ -15,6 +15,10 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Calendar;
 
+/**
+ * A fragment that launches a date picker, either for the end date or start date of an event.
+ * Stores this date in the event and validates the entry fields of the fragment it was called from.
+ */
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
     private TextView dateText;
@@ -25,6 +29,12 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         super();
     }
 
+    /**
+     * constructor for this fragment that stores the references to text, event, and creation fragment.
+     * @param dateText TextView used to update the selected date
+     * @param creatingEvent Event that is being updated with date for
+     * @param eventCreationFragment Fragment that called this date picker to validate entry fields on
+     */
     public DatePickerFragment(TextView dateText, Event creatingEvent, EventCreationFragment eventCreationFragment) {
         super();
         this.dateText = dateText;
@@ -32,6 +42,14 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         this.eventCreationFragment = eventCreationFragment;
     }
 
+    /**
+     * Called when the dialog is created, determines if this is an end date or start date, sets up default date for picker,
+     * uses the date from the event if it had been set previously, and then creates the date picker.
+     * @param savedInstanceState The last saved instance state of the Fragment,
+     * or null if this is a freshly created Fragment.
+     *
+     * @return instance of the date picker with values set
+     */
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -65,6 +83,14 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         return new DatePickerDialog(requireContext(), this, year, month, day);
     }
 
+    /**
+     * Called when date is selected by the picker.
+     * Sets the date in the event and validates the creation fragments entry fields and clears the error for that picker text.
+     * @param view the DatePicker view used
+     * @param year int year selected
+     * @param month int month selected
+     * @param day int day selected
+     */
     public void onDateSet(DatePicker view, int year, int month, int day) {
         // Do something with the time the user picks.
         LocalDate newDate = LocalDate.of(year, month, day);
