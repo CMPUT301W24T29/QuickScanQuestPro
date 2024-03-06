03f3865870d8f9937e3d12a716849a21d8fda27a
ericmah123 <emah2@ualberta.ca>
2024-03-05 22:39:01 -0700

    Trying to fix admin

--- app/src/main/java/com/example/quickscanquestpro/AdminDashboardFragment.java
+++ app/src/main/java/com/example/quickscanquestpro/AdminDashboardFragment.java
@@ -2,12 +2,15 @@ package com.example.quickscanquestpro;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,6 +18,39 @@ import androidx.fragment.app.Fragment;
 import com.example.quickscanquestpro.EventDashboardFragment;
 
 public class AdminDashboardFragment extends Fragment {
+
+
+
+    //User user;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+
+
+    public static AdminDashboardFragment newInstance(String param1, String param2) {
+        AdminDashboardFragment fragment = new AdminDashboardFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     public AdminDashboardFragment() {
         // Required empty public constructor
--- app/src/main/java/com/example/quickscanquestpro/MainActivity.java
+++ app/src/main/java/com/example/quickscanquestpro/MainActivity.java
@@ -90,7 +90,14 @@ public class MainActivity extends AppCompatActivity {
             if (Objects.equals(pressedTitle, dashboardTitle)) {
                 fragment1 = new EventDashboardFragment();
             } else if (Objects.equals(pressedTitle, profileTitle)) {
-                fragment1 = getUser().isAdmin() ? new AdminDashboardFragment() : new ProfileFragment();
+                if (getUser().isAdmin()){
+                    fragment1 = new AdminDashboardFragment();
+                }
+                else{
+                    fragment1 = new ProfileFragment();
+
+                }
+
             } else {
                 // default to qr code home view
                 fragment1 = new HomeViewFragment();
