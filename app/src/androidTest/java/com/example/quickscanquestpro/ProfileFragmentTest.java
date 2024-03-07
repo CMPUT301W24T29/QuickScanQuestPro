472a4e911d16a5b1359c5f27da2ffb86e9335251
iftemum <iftemumraian@gmail.com>
2024-03-06 22:02:46 -0700

    Profile Fragment Test

--- app/src/androidTest/java/com/example/quickscanquestpro/ProfileFragmentTest.java
+++ app/src/androidTest/java/com/example/quickscanquestpro/ProfileFragmentTest.java
@@ -3,33 +3,174 @@ package com.example.quickscanquestpro;
 import static android.app.Activity.RESULT_OK;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.init;
+import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.Intents.release;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static java.util.regex.Pattern.matches;
+
+import android.Manifest;
+import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.net.Uri;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.core.content.ContextCompat;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
 
+import com.google.android.datatransport.backend.cct.BuildConfig;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.init;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.release;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.GrantPermissionRule;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ProfileFragmentTest {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
+//    @Rule
+//    public GrantPermissionRule permissionCamera = GrantPermissionRule.grant(Manifest.permission.CAMERA);
+//    @Rule
+//    public GrantPermissionRule permissionAudio = GrantPermissionRule.grant(Manifest.permission.RECORD_AUDIO);
+//    @Rule
+//    public GrantPermissionRule permissionReadMediaImages = GrantPermissionRule.grant(Manifest.permission.READ_MEDIA_IMAGES);
+//    @Rule
+//    public GrantPermissionRule permissionRead = GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE);
+//    @Rule
+//    public GrantPermissionRule permissionWrite = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+    @Before
+    public void setUp() {
+        init();
+        FirebaseAuth.getInstance().signInAnonymously();
+        // Allow time for authentication to complete
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        FirebaseAuth.getInstance().signOut();
+        release();
+    }
 
     @Test
-    public void navigateToProfileAndUploadPicture() {
+    public void uploadProfilePictureTest() {
+        // Prepare the result data for the gallery intent
+        Intent resultData = new Intent();
+        Uri imageUri = Uri.parse("android.resource://"  + R.drawable.testprofilepicture);
+        resultData.setData(imageUri);
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
 
+
+
+        onView(withId(R.id.navigation_profile)).perform(click());
+
+        // Stub the intent that gets fired when picking an image from the gallery
+        Intents.intending(hasAction(Intent.ACTION_PICK)).respondWith(result);
+
+        // Assume there's a button with ID uploadProfilePictureButton that the user clicks to upload a picture
+        onView(withId(R.id.uploadProfilePictureButton)).perform(click());
+
+        // Check if the action to pick an image was fired
+        intended(hasAction(Intent.ACTION_PICK));
+
+        // Assuming there's a delay while the image is loaded
+        try {
+            Thread.sleep(2000); // It's better to use IdlingResource for synchronization
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // After the image is picked, the ImageView with ID profilePicturePlaceholder should display the image
+        onView(withId(R.id.deleteProfilePictureButton)).check(matches(isDisplayed()));
     }
+
+
 
 
 }
