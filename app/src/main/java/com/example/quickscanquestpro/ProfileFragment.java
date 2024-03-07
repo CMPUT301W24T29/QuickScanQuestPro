e7dfdf774e8ec2fd7abff1685ced68246dae7916
jojojojimmy <joshuajimmy26@gmail.com>
2024-03-07 00:55:53 -0700

    finished the admin profile view

--- app/src/main/java/com/example/quickscanquestpro/ProfileFragment.java
+++ app/src/main/java/com/example/quickscanquestpro/ProfileFragment.java
@@ -48,46 +48,19 @@ public class ProfileFragment extends Fragment {
 
     private DatabaseService databaseService = new DatabaseService();
 
-    //User user;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private User user;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public ProfileFragment(User user) {
+        this.user = user;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         // Initialize the permission request launcher
         requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -136,10 +109,12 @@ public class ProfileFragment extends Fragment {
         EditText emailAddressInput = view.findViewById(R.id.emailAddressInput);
         SwitchMaterial geolocationSwitch = view.findViewById(R.id.geolocationSwitch);
 
+        if(user == null)
+        {
+            MainActivity mainActivity = (MainActivity) getActivity();
+            user = mainActivity.getUser();
+        }
         //Get User from Main activity
-        MainActivity mainActivity = (MainActivity) getActivity();
-        User user = mainActivity.getUser();
-
 
         //Update information
         fullNameInput.addTextChangedListener(new TextWatcher() {
