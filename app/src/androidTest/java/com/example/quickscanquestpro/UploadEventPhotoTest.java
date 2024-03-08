7710695d4d93891781801d74996300b9d1b4cc41
Shawn Belhumeur <sdbelhum@ualberta.ca>
2024-03-07 20:56:08 -0700

    Finished tests for upload photo, working on share QR test

--- app/src/androidTest/java/com/example/quickscanquestpro/UploadEventPhotoTest.java
+++ app/src/androidTest/java/com/example/quickscanquestpro/UploadEventPhotoTest.java
@@ -61,8 +61,9 @@ public class UploadEventPhotoTest extends MainActivityTest{
         release();
     }
 
+    // In the case where the organizer uploads the photo when creating the event
     @Test
-    public void testUS01_04_01UploadEventPhoto() {
+    public void testUS01_04_01UploadEventPhoto_1() {
         // Prepare the result data for the gallery intent
         Intent resultData = new Intent();
         Uri imageUri = Uri.parse("android.resource://com.example.quickscanquestpro/drawable/pork_ribs");
@@ -74,7 +75,7 @@ public class UploadEventPhotoTest extends MainActivityTest{
         // Stub the intent that gets fired when picking an image from the gallery
         Intents.intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(result);
 
-        // Assume there's a button with ID uploadProfilePictureButton that the user clicks to upload a picture
+        // Assume there's a button with ID banner_upload_button that the user clicks to upload a picture
         onView(withId(R.id.banner_upload_button)).perform(click());
 
         // Check if the action to pick an image was fired
@@ -92,6 +93,54 @@ public class UploadEventPhotoTest extends MainActivityTest{
 
         // Confirm creation of event
         onView(withId(R.id.create_event_confirm_button)).perform(click());
+        onView(isRoot()).perform(waitFor(1000));
+
+        // Check if the image is displayed in the event details
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.event_dashboard_list)).atPosition(0).perform(click());
+        onView(isRoot()).perform(waitFor(1000));
+
+        // Check if the image is displayed in the event details
+        onView(withId(R.id.event_banner)).check(matches(isDisplayed()));
+    }
+
+    // In the case where the organizer uploads the photo after the event has already been created
+    @Test
+    public void testUS01_04_01UploadEventPhoto_2() {
+        // Prepare the result data for the gallery intent
+        Intent resultData = new Intent();
+        Uri imageUri = Uri.parse("android.resource://com.example.quickscanquestpro/drawable/pork_ribs");
+        resultData.setData(imageUri);
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
+        createNewEvent();
+
+        // Confirm creation of event
+        onView(withId(R.id.create_event_confirm_button)).perform(click());
+        onView(isRoot()).perform(waitFor(1000));
+
+        // Stub the intent that gets fired when picking an image from the gallery
+        Intents.intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(result);
+
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.event_dashboard_list)).atPosition(0).perform(click());
+        onView(isRoot()).perform(waitFor(1000));
+
+        // Assume there's a button with ID edit_banner_button that the user clicks to upload a picture
+        onView(withId(R.id.edit_banner_button)).perform(click());
+
+        // Check if the action to pick an image was fired
+        intended(hasAction(Intent.ACTION_GET_CONTENT));
+
+        // Assuming there's a delay while the image is loaded
+        try {
+            Thread.sleep(2000); // It's better to use IdlingResource for synchronization
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(isRoot()).perform(waitFor(1000));
+
+        // Click the back button to return to the event dashboard
+        onView(withId(R.id.back_button)).perform(click());
+
         onView(isRoot()).perform(waitFor(1000));
 
         // Check if the image is displayed in the event details
