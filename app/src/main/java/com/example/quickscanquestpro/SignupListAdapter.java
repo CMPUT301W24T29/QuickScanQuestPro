617cd2f40266f0cf2852551d7f8e6c592f12e3e8
josephhdu <joseph.kj.du@gmail.com>
2024-04-06 18:59:06 -0600

    added a signuplist adapter

---
+++ app/src/main/java/com/example/quickscanquestpro/SignupListAdapter.java
@@ -0,0 +1,48 @@
+package com.example.quickscanquestpro;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class SignupListAdapter extends ArrayAdapter<User> {
+    private Context context;
+    private int resource;
+    private List<User> users;
+
+    public SignupListAdapter(@NonNull Context context, int resource, @NonNull List<User> users) {
+        super(context, resource, users);
+        this.context = context;
+        this.resource = resource;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Get the user item for this position
+        User user = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(resource, parent, false);
+        }
+
+        // Lookup view for data population
+        TextView userNameTextView = convertView.findViewById(R.id.signup_name);
+
+        // Populate the data into the template view using the data object
+        userNameTextView.setText(user.getName());
+
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
