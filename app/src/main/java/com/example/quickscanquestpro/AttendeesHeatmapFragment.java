b771fe45022a222136e19324e827b2b4208ac9ba
Alastair <22815637+DrawnActor@users.noreply.github.com>
2024-04-07 18:35:35 -0600

    starting heatmap

---
+++ app/src/main/java/com/example/quickscanquestpro/AttendeesHeatmapFragment.java
@@ -0,0 +1,28 @@
+package com.example.quickscanquestpro;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AttendeesHeatmapFragment extends Fragment {
+
+    public AttendeesHeatmapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_attendees_heatmap, container, false);
+    }
+}
