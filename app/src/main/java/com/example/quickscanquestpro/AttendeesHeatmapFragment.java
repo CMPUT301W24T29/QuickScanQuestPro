5d7c3901997ba67da75adabfd9a63e5b9d2bf0d6
Alastair <22815637+DrawnActor@users.noreply.github.com>
2024-04-07 19:08:08 -0600

    heatmap progress

--- app/src/main/java/com/example/quickscanquestpro/AttendeesHeatmapFragment.java
+++ app/src/main/java/com/example/quickscanquestpro/AttendeesHeatmapFragment.java
@@ -3,10 +3,13 @@ package com.example.quickscanquestpro;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AttendeesHeatmapFragment extends Fragment {
 
@@ -24,5 +27,16 @@ public class AttendeesHeatmapFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_attendees_heatmap, container, false);
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton backButton = view.findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getParentFragmentManager();
+            fragmentManager.popBackStack();
+        });
+
     }
 }
